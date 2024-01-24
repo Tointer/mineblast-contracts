246f8f8baad5cf6b06185ac38f5cb043c154047a
Tointer <35668369+Tointer@users.noreply.github.com>
2024-01-24 23:21:17 +0300

    add updated minichef

---
+++ src/MiniChefV2.sol
@@ -0,0 +1,239 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+/// @notice The (older) MasterChef contract gives out a constant number of SUSHI tokens per block.
+/// It is the only address with minting rights for SUSHI.
+/// The idea for this MasterChef V2 (MCV2) contract is therefore to be the owner of a dummy token
+/// that is deposited into the MasterChef V1 (MCV1) contract.
+/// The allocation point for this pool on MCV1 is the total allocation point for all pools that receive double incentives.
+contract MiniChefV2 is Ownable{
+    /// @notice Info of each MCV2 user.
+    /// `amount` LP token amount the user has provided.
+    /// `rewardDebt` The amount of SUSHI entitled to the user.
+    struct UserInfo {
+        uint256 amount;
+        int256 rewardDebt;
+    }
+
+    /// @notice Info of each MCV2 pool.
+    /// `allocPoint` The amount of allocation points assigned to the pool.
+    /// Also known as the amount of SUSHI to distribute per block.
+    struct PoolInfo {
+        uint128 accSushiPerShare;
+        uint64 lastRewardTime;
+        uint64 allocPoint;
+    }
+
+    /// @notice Address of SUSHI contract.
+    IERC20 public immutable SUSHI;
+
+    /// @notice Info of each MCV2 pool.
+    PoolInfo[] public poolInfo;
+    /// @notice Address of the LP token for each MCV2 pool.
+    IERC20[] public lpToken;
+
+    /// @notice Info of each user that stakes LP tokens.
+    mapping (uint256 => mapping (address => UserInfo)) public userInfo;
+
+    /// @dev Tokens added
+    mapping (address => bool) public addedTokens;
+
+    /// @dev Total allocation points. Must be the sum of all allocation points in all pools.
+    uint256 public totalAllocPoint;
+
+    uint256 public sushiPerSecond;
+    uint256 private constant ACC_SUSHI_PRECISION = 1e12;
+
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount, address indexed to);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount, address indexed to);
+    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount, address indexed to);
+    event Harvest(address indexed user, uint256 indexed pid, uint256 amount);
+    event LogPoolAddition(uint256 indexed pid, uint256 allocPoint, IERC20 indexed lpToken);
+    event LogSetPool(uint256 indexed pid, uint256 allocPoint);
+    event LogUpdatePool(uint256 indexed pid, uint64 lastRewardTime, uint256 lpSupply, uint256 accSushiPerShare);
+    event LogSushiPerSecond(uint256 sushiPerSecond);
+
+    /// @param _sushi The SUSHI token contract address.
+    constructor(IERC20 _sushi) Ownable(msg.sender) {
+        SUSHI = _sushi;
+    }
+
+    /// @notice Returns the number of MCV2 pools.
+    function poolLength() public view returns (uint256 pools) {
+        pools = poolInfo.length;
+    }
+
+    /// @notice Add a new LP to the pool. Can only be called by the owner.
+    /// DO NOT add the same LP token more than once. Rewards will be messed up if you do.
+    /// @param allocPoint AP of the new pool.
+    /// @param _lpToken Address of the LP ERC-20 token.
+    function add(uint256 allocPoint, IERC20 _lpToken) public onlyOwner {
+        require(addedTokens[address(_lpToken)] == false, "Token already added");
+        totalAllocPoint = totalAllocPoint + allocPoint;
+        lpToken.push(_lpToken);
+
+        poolInfo.push(PoolInfo({
+            allocPoint: uint64(allocPoint),
+            lastRewardTime: uint64(block.timestamp),
+            accSushiPerShare: 0
+        }));
+        addedTokens[address(_lpToken)] = true;
+        emit LogPoolAddition(lpToken.length - 1, allocPoint, _lpToken);
+    }
+
+    /// @notice Update the given pool's SUSHI allocation point. Can only be called by the owner.
+    /// @param _pid The index of the pool. See `poolInfo`.
+    /// @param _allocPoint New AP of the pool.
+    function set(uint256 _pid, uint256 _allocPoint) public onlyOwner {
+        totalAllocPoint = totalAllocPoint - poolInfo[_pid].allocPoint + _allocPoint;
+        poolInfo[_pid].allocPoint = uint64(_allocPoint);
+
+        emit LogSetPool(_pid, _allocPoint);
+    }
+
+    /// @notice Sets the sushi per second to be distributed. Can only be called by the owner.
+    /// @param _sushiPerSecond The amount of Sushi to be distributed per second.
+    function setSushiPerSecond(uint256 _sushiPerSecond) public onlyOwner {
+        sushiPerSecond = _sushiPerSecond;
+        emit LogSushiPerSecond(_sushiPerSecond);
+    }
+
+    /// @notice View function to see pending SUSHI on frontend.
+    /// @param _pid The index of the pool. See `poolInfo`.
+    /// @param _user Address of user.
+    /// @return pending SUSHI reward for a given user.
+    function pendingSushi(uint256 _pid, address _user) external view returns (uint256 pending) {
+        PoolInfo memory pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accSushiPerShare = pool.accSushiPerShare;
+        uint256 lpSupply = lpToken[_pid].balanceOf(address(this));
+        if (block.timestamp > pool.lastRewardTime && lpSupply != 0) {
+            uint256 time = block.timestamp - pool.lastRewardTime;
+            uint256 sushiReward = time * sushiPerSecond * pool.allocPoint / totalAllocPoint;
+            accSushiPerShare = accSushiPerShare + sushiReward * ACC_SUSHI_PRECISION / lpSupply;
+        }
+        pending = uint(int256(user.amount * accSushiPerShare / ACC_SUSHI_PRECISION) - user.rewardDebt);
+    }
+
+    /// @notice Update reward variables for all pools. Be careful of gas spending!
+    /// @param pids Pool IDs of all to be updated. Make sure to update all active pools.
+    function massUpdatePools(uint256[] calldata pids) external {
+        uint256 len = pids.length;
+        for (uint256 i = 0; i < len; ++i) {
+            updatePool(pids[i]);
+        }
+    }
+
+    /// @notice Update reward variables of the given pool.
+    /// @param pid The index of the pool. See `poolInfo`.
+    /// @return pool Returns the pool that was updated.
+    function updatePool(uint256 pid) public returns (PoolInfo memory pool) {
+        pool = poolInfo[pid];
+        if (block.timestamp > pool.lastRewardTime) {
+            uint256 lpSupply = lpToken[pid].balanceOf(address(this));
+            if (lpSupply > 0) {
+                uint256 time = block.timestamp - pool.lastRewardTime;
+                uint256 sushiReward = time * sushiPerSecond * pool.allocPoint / totalAllocPoint;
+                pool.accSushiPerShare = uint128(pool.accSushiPerShare + sushiReward * ACC_SUSHI_PRECISION / lpSupply);
+            }
+            pool.lastRewardTime = uint64(block.timestamp);
+            poolInfo[pid] = pool;
+            emit LogUpdatePool(pid, pool.lastRewardTime, lpSupply, pool.accSushiPerShare);
+        }
+    }
+
+    /// @notice Deposit LP tokens to MCV2 for SUSHI allocation.
+    /// @param pid The index of the pool. See `poolInfo`.
+    /// @param amount LP token amount to deposit.
+    /// @param to The receiver of `amount` deposit benefit.
+    function deposit(uint256 pid, uint256 amount, address to) public {
+        PoolInfo memory pool = updatePool(pid);
+        UserInfo storage user = userInfo[pid][to];
+
+        // Effects
+        user.amount = user.amount + amount;
+        user.rewardDebt = user.rewardDebt + int256(amount * pool.accSushiPerShare / ACC_SUSHI_PRECISION);
+
+        lpToken[pid].transferFrom(msg.sender, address(this), amount);
+
+        emit Deposit(msg.sender, pid, amount, to);
+    }
+
+    /// @notice Withdraw LP tokens from MCV2.
+    /// @param pid The index of the pool. See `poolInfo`.
+    /// @param amount LP token amount to withdraw.
+    /// @param to Receiver of the LP tokens.
+    function withdraw(uint256 pid, uint256 amount, address to) public {
+        PoolInfo memory pool = updatePool(pid);
+        UserInfo storage user = userInfo[pid][msg.sender];
+
+        // Effects
+        user.rewardDebt = user.rewardDebt - (int256(amount * pool.accSushiPerShare / ACC_SUSHI_PRECISION));
+        user.amount = user.amount - amount;
+
+        lpToken[pid].transfer(to, amount);
+
+        emit Withdraw(msg.sender, pid, amount, to);
+    }
+
+    /// @notice Harvest proceeds for transaction sender to `to`.
+    /// @param pid The index of the pool. See `poolInfo`.
+    /// @param to Receiver of SUSHI rewards.
+    function harvest(uint256 pid, address to) public {
+        PoolInfo memory pool = updatePool(pid);
+        UserInfo storage user = userInfo[pid][msg.sender];
+        int256 accumulatedSushi = int256(user.amount * pool.accSushiPerShare / ACC_SUSHI_PRECISION);
+        uint256 _pendingSushi = uint(accumulatedSushi - user.rewardDebt);
+
+        // Effects
+        user.rewardDebt = accumulatedSushi;
+
+        // Interactions
+        if (_pendingSushi != 0) {
+            SUSHI.transfer(to, _pendingSushi);
+        }
+
+        emit Harvest(msg.sender, pid, _pendingSushi);
+    }
+
+    /// @notice Withdraw LP tokens from MCV2 and harvest proceeds for transaction sender to `to`.
+    /// @param pid The index of the pool. See `poolInfo`.
+    /// @param amount LP token amount to withdraw.
+    /// @param to Receiver of the LP tokens and SUSHI rewards.
+    function withdrawAndHarvest(uint256 pid, uint256 amount, address to) public {
+        PoolInfo memory pool = updatePool(pid);
+        UserInfo storage user = userInfo[pid][msg.sender];
+        int256 accumulatedSushi = int256(user.amount * pool.accSushiPerShare / ACC_SUSHI_PRECISION);
+        uint256 _pendingSushi = uint(accumulatedSushi -user.rewardDebt);
+
+        // Effects
+        user.rewardDebt = accumulatedSushi - (int256(amount * pool.accSushiPerShare / ACC_SUSHI_PRECISION));
+        user.amount = user.amount - amount;
+
+        // Interactions
+        SUSHI.transfer(to, _pendingSushi);
+
+        lpToken[pid].transfer(to, amount);
+
+        emit Withdraw(msg.sender, pid, amount, to);
+        emit Harvest(msg.sender, pid, _pendingSushi);
+    }
+
+    /// @notice Withdraw without caring about rewards. EMERGENCY ONLY.
+    /// @param pid The index of the pool. See `poolInfo`.
+    /// @param to Receiver of the LP tokens.
+    function emergencyWithdraw(uint256 pid, address to) public {
+        UserInfo storage user = userInfo[pid][msg.sender];
+        uint256 amount = user.amount;
+        user.amount = 0;
+        user.rewardDebt = 0;
+
+        // Note: transfer can fail or succeed if `amount` is zero.
+        lpToken[pid].transfer(to, amount);
+        emit EmergencyWithdraw(msg.sender, pid, amount, to);
+    }
+}
