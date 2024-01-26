b5164241eafb3fccaefa878db5c6cca1e81e921a
Tointer <35668369+Tointer@users.noreply.github.com>
2024-01-26 22:21:20 +0300

    rename

--- src/swap/MineblastPairFactory.sol
+++ src/swap/MineblastPairFactory.sol
@@ -3,7 +3,7 @@ pragma solidity =0.5.16;
 import './interfaces/IUniswapV2Factory.sol';
 import './MineblastSwap.sol';
 
-contract UniswapV2Factory is IUniswapV2Factory {
+contract MineblastPairFactory is IUniswapV2Factory {
     address public feeTo;
     address public feeToSetter;
 
