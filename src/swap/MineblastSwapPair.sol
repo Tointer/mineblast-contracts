9bfbf482c0dcbe96cd5b70267b1ad7524527aedd
Tointer <35668369+Tointer@users.noreply.github.com>
2024-01-27 18:35:53 +0300

    make swap function rebasing-friendly

--- src/swap/MineblastSwapPair.sol
+++ src/swap/MineblastSwapPair.sol
@@ -141,10 +141,16 @@ contract MineblastSwapPair is IUniswapV2Pair, UniswapV2ERC20 {
     }
 
     // this low-level function should be called from a contract which performs important safety checks
-    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external lock {
+    function swap(uint token0AmountIn, uint token1AmountIn, uint amount0Out, uint amount1Out, address to, bytes calldata data) external lock {
         require(amount0Out > 0 || amount1Out > 0, 'UniswapV2: INSUFFICIENT_OUTPUT_AMOUNT');
+        require(token0AmountIn > 0 || token1AmountIn > 0, 'UniswapV2: INSUFFICIENT_INPUT_AMOUNT');
+        sync(); //sync to account for rebasing rewards and not gifting them to swapper
         (uint112 _reserve0, uint112 _reserve1,) = getReserves(); // gas savings
         require(amount0Out < _reserve0 && amount1Out < _reserve1, 'UniswapV2: INSUFFICIENT_LIQUIDITY');
+
+        //because of sync we can't expect transfers uniswap's way, need to transfer them here manually 
+        if (token0AmountIn > 0) IERC20(token0).transferFrom(msg.sender, address(this), token0AmountIn);
+        if (token1AmountIn > 0) IERC20(token1).transferFrom(msg.sender, address(this), token1AmountIn);
 
         uint balance0;
         uint balance1;
@@ -155,20 +161,21 @@ contract MineblastSwapPair is IUniswapV2Pair, UniswapV2ERC20 {
         if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
         if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
         if (data.length > 0) IUniswapV2Callee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data);
-        balance0 = IERC20(_token0).balanceOf(address(this));
-        balance1 = IERC20(_token1).balanceOf(address(this));
+
+        // reserves is equal to balances before transfers because of sync
+        // BUT this will break if token0 or token1 have fee on transfer or something like that
+        balance0 = _reserve0 + token0AmountIn - amount0Out;
+        balance1 = _reserve1 + token1AmountIn - amount1Out;
         }
-        uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
-        uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
-        require(amount0In > 0 || amount1In > 0, 'UniswapV2: INSUFFICIENT_INPUT_AMOUNT');
+
         { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
-        uint balance0Adjusted = balance0.mul(1000).sub(amount0In.mul(3));
-        uint balance1Adjusted = balance1.mul(1000).sub(amount1In.mul(3));
+        uint balance0Adjusted = balance0.mul(1000).sub(token0AmountIn.mul(3));
+        uint balance1Adjusted = balance1.mul(1000).sub(token1AmountIn.mul(3));
         require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(1000**2), 'UniswapV2: K');
         }
 
         _update(balance0, balance1, _reserve0, _reserve1);
-        emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
+        emit Swap(msg.sender, token0AmountIn, token1AmountIn, amount0Out, amount1Out, to);
     }
 
     // force reserves to match balances
