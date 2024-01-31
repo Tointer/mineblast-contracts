fbdd9846cae2c1278741fef775be1e23e2a0145a
Tointer <35668369+Tointer@users.noreply.github.com>
2024-01-31 22:39:58 +0300

    add blast erc20 contract (with gas claim)

---
+++ src/BlastERC20.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {ERC20} from 'solmate/tokens/ERC20.sol';
+import './blast/IBlast.sol';
+
+contract BlastERC20 is ERC20{
+
+    IBlast public constant BLAST = IBlast(0x4300000000000000000000000000000000000002);
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint totalSupply,
+        address claimer
+    ) ERC20(_name, _symbol, 18){
+        BLAST.configure(YieldMode.CLAIMABLE, GasMode.CLAIMABLE, claimer);
+
+        _mint(msg.sender, totalSupply);
+    }
+}
