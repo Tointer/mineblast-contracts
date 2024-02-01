f20522cfe0a275d2079e870fa00ce746c677a398
Tointer <35668369+Tointer@users.noreply.github.com>
2024-02-01 06:19:27 +0300

    refactor + add lp providing test

--- test/MineblastVault.t.sol
+++ test/MineblastVault.t.sol
@@ -10,16 +10,21 @@ import {MineblastFactory} from "../src/MineblastFactory.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../src/mocks/BlastMock.sol";
 import "../src/mocks/RebasingWETHMock.sol";
+import "../src/MineblastVault.sol";
 
 contract MineblastSwapPairTest is Test {
     MineblastSwapPairFactory public swapPairFactory;
     MineblastFactory public mineblastFactory;
+    RebasingWETHMock public wethMock;
 
     address public coinCreator = address(0x1);
     address public protocolCreator = address(0xaaa);
+    address public user1 = address(0x2);
 
     function setUp() public {
         setUpBlastEnv();
+
+        wethMock.mint(user1, 1e21);
 
         vm.startPrank(protocolCreator);
         swapPairFactory = new MineblastSwapPairFactory();
@@ -27,32 +32,12 @@ contract MineblastSwapPairTest is Test {
         vm.stopPrank();
     }
 
-    function test_create_with_new_token() public {
-        uint supply = 1e21;
-        uint16 creatorShare = 1000;
-        uint64 duration = 10000;
-
-        vm.startPrank(coinCreator);
-
-        (address vaultAddress, address tokenAddress) = 
-            mineblastFactory.createVaultWithNewToken(supply, "TOKEN0", "TKN0", duration, creatorShare);
-
-        vm.stopPrank();
-    }
-
     function test_create_supply() public {
         uint supply = 1e21;
         uint16 creatorShare = 1000;
         uint64 duration = 10000;
-
-        vm.startPrank(coinCreator);
-
-        (address vaultAddress, address tokenAddress) = 
-            mineblastFactory.createVaultWithNewToken(supply, "TOKEN0", "TKN0", duration, creatorShare);
-
-        vm.stopPrank();
-
-        IERC20 token = IERC20(tokenAddress);
+        (MineblastVault vault, MineblastSwapPair swapPair, IERC20 token) 
+            = createVault(supply, creatorShare, duration);
 
         uint protocolBaseShareSetting = mineblastFactory.baseProtocolShareBps();
         uint protocolOwnerShareSetting = mineblastFactory.protocolShareFromOwnerShareBps();
@@ -66,7 +51,50 @@ contract MineblastSwapPairTest is Test {
         assertEq(token.totalSupply(), supply);
         assertEq(token.balanceOf(coinCreator), ownerShare);
         assertEq(token.balanceOf(protocolCreator), protocolShare);
-        assertEq(token.balanceOf(vaultAddress), supply - ownerShare - protocolShare);
+        assertEq(token.balanceOf(address(vault)), supply - ownerShare - protocolShare);
+    }
+
+    
+    function test_farm_and_harvest() public {
+        uint supply = 1e21;
+        uint16 creatorShare = 0;
+        uint64 duration = 10000;
+        (MineblastVault vault, MineblastSwapPair swapPair, IERC20 token) 
+            = createVault(supply, creatorShare, duration);
+
+        vm.startPrank(user1);
+        wethMock.approve(address(vault), 1e21);
+        vault.deposit(0, 1e20, user1);
+        vm.roll(block.number + 1);
+        vm.warp(block.timestamp + duration/2);
+        vault.harvest(0, user1);
+        vm.stopPrank();
+
+        uint rewardSupply = supply - (supply * mineblastFactory.baseProtocolShareBps() / 10000);
+
+        assertEq(token.balanceOf(user1), rewardSupply * 5000 / duration);
+    }
+
+    function test_liqudity_add_empty_pool() public {
+        uint supply = 1e21;
+        uint16 creatorShare = 0;
+        uint64 duration = 10000;
+        (MineblastVault vault, MineblastSwapPair swapPair, IERC20 token) 
+            = createVault(supply, creatorShare, duration);
+
+        wethMock.setClaimable(address(vault), 1e17);
+
+        vm.startPrank(user1);
+        wethMock.approve(address(vault), 1e21);
+        vault.deposit(0, 1e20, user1);
+        vm.roll(block.number + 1);
+        vm.warp(block.timestamp + 5000);
+        vault.harvest(0, user1);
+        vm.stopPrank();
+
+        uint expectedLpSupply = sqrt(1e17*1e18);
+        assertEq(swapPair.totalSupply(), expectedLpSupply);
+        assertEq(swapPair.balanceOf(address(vault)), expectedLpSupply - 1000);
     }
 
     function setUpBlastEnv() public{
@@ -75,9 +103,40 @@ contract MineblastSwapPairTest is Test {
         address blastTargetAddress = address(0x4300000000000000000000000000000000000002);
         vm.etch(blastTargetAddress, blastCode);
 
-        RebasingWETHMock wethMock = new RebasingWETHMock();
-        bytes memory wethCode = address(wethMock).code;
+        RebasingWETHMock wethMockContract = new RebasingWETHMock();
+        bytes memory wethCode = address(wethMockContract).code;
         address wethTargetAddress = address(0x4200000000000000000000000000000000000023);
         vm.etch(wethTargetAddress, wethCode);
+        wethMock = RebasingWETHMock(payable(wethTargetAddress));
+    }
+
+    function createVault(
+        uint supply, 
+        uint16 creatorShare, 
+        uint64 duration
+    ) public returns (MineblastVault vault, MineblastSwapPair swapPair, IERC20 token) {
+        vm.startPrank(coinCreator);
+
+        (address vaultAddress, address swapPairAddress, address tokenAddress) = 
+            mineblastFactory.createVaultWithNewToken(supply, "TOKEN0", "TKN0", duration, creatorShare);
+
+        vm.stopPrank();
+
+        vault = MineblastVault(vaultAddress);
+        swapPair = MineblastSwapPair(swapPairAddress);
+        token = IERC20(tokenAddress);
+    }
+
+    function sqrt(uint y) internal pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
     }
 }
