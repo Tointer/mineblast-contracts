1b2c7aeebf0804bf7163932b021a203138c0d9e0
Tointer <35668369+Tointer@users.noreply.github.com>
2024-02-02 18:39:09 +0300

    update hardhat config to work with blast network & scan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,11 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-foundry";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
 import '@typechain/hardhat';
 
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -13,17 +16,35 @@ const config: HardhatUserConfig = {
           viaIR: true,
           optimizer: {
             enabled: true,
-            runs: 999999
+            runs: 1000000
           }
         },
-      },
-      {
-        version: "0.5.16",
       },
     ],
   },
   paths: {
     tests: "./test/hardhat-tests",
+  },
+  etherscan: {
+    apiKey: {
+      blast_sepolia: "blast_sepolia", // apiKey is not required, just set a placeholder
+    },
+    customChains: [
+      {
+        network: "blast_sepolia",
+        chainId: 168587773,
+        urls: {
+          apiURL: "https://api.routescan.io/v2/network/testnet/evm/168587773/etherscan",
+          browserURL: "https://testnet.blastscan.io"
+        }
+      }
+    ]
+  },
+  networks: {
+    blast_sepolia: {
+      url: 'https://sepolia.blast.io',
+      accounts: [process.env.PRIVATE_KEY || ''],
+    },
   },
 };
 
