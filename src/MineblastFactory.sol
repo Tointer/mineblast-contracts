b8d532136df641b0ec9754c6d8ebaace49494756
Tointer <35668369+Tointer@users.noreply.github.com>
2024-02-14 13:25:53 +0300

    add ownerShare to factory struct, add offchain helpers

--- src/MineblastFactory.sol
+++ src/MineblastFactory.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.23;
 import "./MineblastVault.sol";
 import "./BlastERC20.sol";
 import "./swap/interfaces/IMineblastSwapPairFactory.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import './swap/MineblastSwapPair.sol';
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MineblastFactory is Ownable{
@@ -23,30 +23,29 @@ contract MineblastFactory is Ownable{
         address vault;
         address pair;
         address token;
+        uint16 ownerShareBps;
     }
 
     constructor(address _swapPairFactory) Ownable(msg.sender) {
         swapPairFactory = IMineblastSwapPairFactory(_swapPairFactory);
     }
 
-    // function createVaultWithExistingToken(
-    //     uint amount,
-    //     address token, 
-    //     uint64 duration
-    // ) external returns (address) {
-    //     require(amount > 0, "Amount must be greater than 0");
+    function getAllVaultsLength() external view returns(uint64){
+        return uint64(allVaults.length);
+    }
 
-    //     address pairAddress = getPairCreateIfNeeded(wethAddress, token);
-    //     MineblastVault vault = new MineblastVault(token, pairAddress, duration);
-
-    //     uint protocolCut = amount * baseProtocolShareBps / 10000;
-    //     uint finalAmount = amount - protocolCut;
-
-    //     IERC20(token).transfer(owner(), protocolCut);
-    //     IERC20(token).approve(address(vault), finalAmount);
-    //     vault.initialize(finalAmount);
-    //     return address(vault);
-    // }
+    function getAllVaults(uint64 maxElements, uint64 offset) external view returns(VaultInfo[] memory){
+        require(offset < allVaults.length, "Offset is out of bounds");
+        uint64 length = maxElements;
+        if (offset + maxElements > allVaults.length) {
+            length = uint64(allVaults.length - offset);
+        }
+        VaultInfo[] memory result = new VaultInfo[](length);
+        for (uint64 i = 0; i < length; i++) {
+            result[i] = allVaults[offset + i];
+        }
+        return result;
+    }
 
     function createVaultWithNewToken(
         uint supply,
@@ -58,7 +57,7 @@ contract MineblastFactory is Ownable{
         require(supply > 0, "supply must be greater than 0");
        
         BlastERC20 token = new BlastERC20(name, symbol, supply, msg.sender);
-        pairAddress = getPairCreateIfNeeded(wethAddress, address(token));
+        pairAddress =  swapPairFactory.createPair(wethAddress, address(token));
         MineblastVault vault = new MineblastVault(address(token), pairAddress, duration);
 
         (uint ownerSupply, uint protocolCut,  uint finalAmount) = calculateShares(supply, ownerSupplyBps);
@@ -69,7 +68,7 @@ contract MineblastFactory is Ownable{
         token.approve(address(vault), finalAmount);
         vault.initialize(finalAmount);
 
-        allVaults.push(VaultInfo(address(vault), pairAddress, address(token)));
+        allVaults.push(VaultInfo(address(vault), pairAddress, address(token), ownerSupplyBps));
         emit VaultCreated(address(vault), pairAddress, address(token));
         
         return (address(vault), pairAddress, address(token));
@@ -82,21 +81,9 @@ contract MineblastFactory is Ownable{
         require(ownerSupplyBps <= maxOwnerShareBps, "Owner supply must be less than or equal to the max owner share");
         
         ownerSupply = supply * ownerSupplyBps / 10000;
-        uint protocolBaseCut = supply * baseProtocolShareBps / 10000;
         uint protocolOwnerCut = ownerSupply * protocolShareFromOwnerShareBps / 10000;
         ownerSupply = ownerSupply - protocolOwnerCut;
-        protocolCut = protocolBaseCut + protocolOwnerCut;
-        finalAmount = supply - ownerSupply - protocolCut;
-    }
-
-    function getPairCreateIfNeeded(
-        address tokenA, 
-        address tokenB
-    ) internal returns (address) {
-        address existingPool = swapPairFactory.getPair(tokenA, tokenB);
-        if (existingPool == address(0)) {
-            return swapPairFactory.createPair(tokenA, tokenB);
-        }
-        return existingPool;
+        protocolCut = supply * baseProtocolShareBps / 10000 + protocolOwnerCut;
+        finalAmount = supply - supply * baseProtocolShareBps / 10000 - protocolOwnerCut - ownerSupply;
     }
 }
